19e9195287805a875d5c51f1b14a498924c02816
systemfile36 <systemfile36@gmail.com>
2024-09-11 14:43:54 +0900

    Initial commit

---
+++ src/main/java/org/diarymoodanalyzer/DiaryMoodAnalyzerApplication.java
@@ -0,0 +1,11 @@
+package org.diarymoodanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryMoodAnalyzerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryMoodAnalyzerApplication.class, args);
+    }
+}
