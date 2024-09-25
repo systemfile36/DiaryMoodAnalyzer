af1df612d077993fe4389fb30b5fa374c8637a3c
배형찬 <chan941027@gmail.com>
2024-09-25 15:22:08 +0900

    Update DiaryMoodAnalyzerApplication.java
    test

--- src/main/java/org/diarymoodanalyzer/DiaryMoodAnalyzerApplication.java
+++ src/main/java/org/diarymoodanalyzer/DiaryMoodAnalyzerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//test123
+
 @EnableJpaAuditing //@CreatedDate, @LastModifiedDate 자동 반영 위함
 @SpringBootApplication
 public class DiaryMoodAnalyzerApplication {
