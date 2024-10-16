a0e95a1cc7656e9bf3f4eac36293326fb840fe9b
systemfile36 <84090525+systemfile36@users.noreply.github.com>
2024-10-16 23:39:39 +0900

    feat: TempDiaryList 스타일 변경. DiaryManager에 로직과 스테이트 추가(모듈화를 통해 확장성 상승을 위함.). 기타 수정.

--- frontend/src/components/TempDiaryList.vue
+++ frontend/src/components/TempDiaryList.vue
@@ -1,17 +1,24 @@
 <template>
-    <div id="diary-list" class="flex-grow-1 px-md-4">
-        <article class="card-wrapper card" v-for="(diary, i) in diaryManager.diaries" :key="i">
+    <div id="diary-list" class="flex-grow-1 px-4">
+        <article class="card-wrapper card" v-for="(diary, i) in diaries" :key="i">
             <router-link to="#" class="diary-preview row g-0 flex-md-row-reverse">
                 <div class="col-md-12">
                     <div class="card-body d-flex flex-column">
-                        <h1 class="card-title my-2 mt-md-0 text-primary-emphasis">{{ diary.title }}</h1>
+                        <h1 class="card-title my-2 mt-md-0 text-primary-emphasis">
+                            {{ getTruncated(diary.title, 50) }}
+                        </h1>
                         <div class="card-text content mt-0 mb-3 text-secondary">
-                            <p> {{ diary.content }} </p>
+                            <p class="text-clip-2"> {{ getTruncated(diary.content) }} </p>
                         </div>
                         <div class="diary-meta flex-grow-1 d-flex align-items-end text-primary-emphasis">
                             <div class="me-auto">
                                 <i class="fa-regular fa-calendar"></i>
-                                <time>{{ new Date(diary.createdAt).toISOString().split('T')[0] }}</time>
+                                <time>
+                                    {{ diaryManager.formatDate(diary.createdAt) }}
+                                </time>
+                                <!-- 감정 분석 결과 -->
+                                <i class="fa-regular fa-face-smile"></i>
+                                <span class="sentimental">not bad</span>
                             </div>
                         </div>
                     </div>
@@ -22,26 +29,56 @@
 </template>
 <script setup>
 import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 import { useDiaryManagerStore } from '../stores/DiaryManager';
 
 const diaryManager = useDiaryManagerStore();
 
+//반응형을 유지하기 위해 destructuring 해서 받아옴 
+const { diaries } = storeToRefs(diaryManager);
+
 onMounted(()=>{
-    diaryManager.getDiaries();
+    diaryManager.loadDiaries();
     console.log(`diaries : ${diaryManager.diaries}`);
 })
 
+ /**
+  * maxLength 만큼 자른 값을 반환한다. 
+  * 만약 길이가 maxLength보다 작다면 그대로 리턴한다.
+  * @param {string} content 
+  * @param {number} maxLength 최대 길이. 기본값 140
+  * @returns {string}
+  */
+function getTruncated(content, maxLength = 140) {
+
+    //길이가 최대 길이를 넘으면 잘라서, 그렇지 않으면 그냥 리턴
+    return content.length > maxLength ? 
+        content.substring(0, maxLength) + '...' : content;
+}
+
 </script>
-<style lang="scss">
+<style scoped lang="scss">
     #diary-list {
         margin-top: 2.5rem;
 
+        //a 태그의 기본 값인 텍스트 밑줄 제거
         * {
             text-decoration: none;
         }
 
-        .diary-preview {
+        //두 줄을 넘는 콘텐츠 텍스트는 ...으로 truncated 한다.
+        //webkit을 사용한다. 
+        .text-clip-2 {
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            line-clamp: 2;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
 
+        //다이어리 미리보기 창 
+        .diary-preview {
             //배경 가상 요소. opacity의 변화에 따라 transition
             //&:를 사용해서 .diary-preview의 가상요소에만 적용(안그러면 )
             &:before {
@@ -49,12 +86,12 @@ onMounted(()=>{
                 width: 100%;
                 height: 100%;
                 position: absolute;
-                background-color: var(--bs-secondary-bg);
+                background-color: var(--bs-dark);
                 opacity: 0;
                 transition: opacity, .35s ease-in-out;
             }
 
-            //
+            //배경 요소에 hover하면 투명도 올림 (호버 강조 효과)
             &:hover {
                 &::before {
                     opacity: 0.3;
@@ -62,17 +99,23 @@ onMounted(()=>{
             }
 
             background-color: var(--bs-body-bg);
-            border-radius: .625rem;
             
         }
 
+        //마지막 요소가 아니라면 아래에 margin
         .card-wrapper:not(:last-child) {
             margin-bottom: 1.25rem;
         }
 
         .diary-meta {
-            time {
+            //아이콘과 거리 두기 
+            time, span {
                 padding-left: 0.5rem;
+            }
+
+            //첫번째 요소가 아닌 i(아이콘)의 왼쪽 margin을 설정
+            i:not(:first-child) {
+                margin-left: 1.5rem;
             }
         }
     }
--- frontend/src/stores/DiaryManager.js
+++ frontend/src/stores/DiaryManager.js
@@ -4,27 +4,46 @@ import { useRouter } from 'vue-router'
 import axios from 'axios'
 import TokenUtils from '../utils/TokenUtils'
 import { useAuthManagerStore } from './AuthManager'
+import dayjs from 'dayjs';
 
+/**
+ * 다이어리와 관련된 상태와 함수들이 정의된 Pinia 저장소 
+ */
 export const useDiaryManagerStore = defineStore('diaryManager', ()=>{
     const diariesUrl = '/api/diaries';
     const diaryUrl = '/api/diary';
     const diariesTitleUrl = '/api/diaries/title';
 
+    /**
+     * 다이어리의 날짜 포맷 형식 
+     */
+    const diaryDateFormat = 'YYYY-MM-DD HH:mm:ss';
 
+    //페이징 관련 변수
     const currentPage = ref(0);
     const currentPageSize = ref(10);
     const isAscending = ref(false);
+
+    const maxTitleLength = ref(50);
+    const maxContentLength = ref(500);
     
     //나중에 열거형 상수로 정의할 예정 
     const sortBy = ref('createdAt');
 
-    const diariesTitle = ref([]);
+    /**
+     * 다이어리 목록이 저장될 배열
+     */
     const diaries = ref([]);
 
     const router = useRouter();
 
     const authManager = useAuthManagerStore();
 
+    /**
+     * 제목과 내용을 받아서 다이어리를 추가함 
+     * @param {string} title 
+     * @param {string} content 
+     */
     async function addDiary(title, content) {
         const diary = {
             title: title,
@@ -43,23 +62,10 @@ export const useDiaryManagerStore = defineStore('diaryManager', ()=>{
             })
     }
 
-    async function getDiariesTitle() {
-        await axios.get(diariesTitleUrl, {
-            headers: authManager.getDefaultHeaders(),
-            params: getPagingParams(),
-        }).then((res) => {
-            console.log(res);
-
-            //store 변수에 설정 
-            diariesTitle.value = res.data.content;
-
-            console.log(diariesTitle.value);
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
-
-    async function getDiaries() {
+    /**
+     * diaries 변수에 현재 인증된 유저의 다이어리들을 로드한다.
+     */
+    async function loadDiaries() {
         await axios.get(diariesUrl, {
             headers: authManager.getDefaultHeaders(),
             params: getPagingParams(),
@@ -74,7 +80,10 @@ export const useDiaryManagerStore = defineStore('diaryManager', ()=>{
         })
     }
 
-
+    /**
+     * 페이징에 필요한 파라미터를 반환한다.
+     * @returns 페이징 파라미터
+     */
     function getPagingParams() {
         return {
             page: currentPage.value,
@@ -84,15 +93,41 @@ export const useDiaryManagerStore = defineStore('diaryManager', ()=>{
         };
     }
 
+    /**
+     * 날짜를 store에 저장된 포맷으로 변환 후 반환
+     * @param {string} date - datetime 문자열
+     * @returns formatted datetime string
+     */
+    function formatDate(date) {
+        //day.js 사용 
+        return dayjs(date).format(diaryDateFormat); // 날짜 포맷 설정
+    }
+
+     /**
+      * maxLength 만큼 자른 값을 반환한다. 
+      * 만약 길이가 maxLength보다 작다면 그대로 리턴한다.
+      * @param {string} content 
+      * @param {number} maxLength 최대 길이. 기본값 140
+      * @returns {string}
+      */
+    function getTruncated(content, maxLength = 140) {
+
+        //길이가 최대 길이를 넘으면 잘라서, 그렇지 않으면 그냥 리턴
+        return content.length > maxLength ? 
+            content.substring(0, maxLength) + '...' : content;
+    }
+
     return {
         currentPage,
         currentPageSize,
         isAscending,
         sortBy,
-        diariesTitle,
         diaries,
+        maxTitleLength,
+        maxContentLength,
         addDiary,
-        getDiariesTitle,
-        getDiaries,
+        loadDiaries,
+        formatDate,
+        getTruncated,
     }
 })
